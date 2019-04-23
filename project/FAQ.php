a2bd18a5f8552a6e6d2630879441e695805621b0
tiiiiiiiffffany <48717345+tiiiiiiiffffany@users.noreply.github.com>
2019-04-23 19:55:49 +0800

    Update FAQ.php

--- project/FAQ.php
+++ project/FAQ.php
@@ -3,3 +3,28 @@ I will contact you through your gmail as soon as possible
 Have a nice day
 Regards,
 Tiffany Chen
+
+<?php
+
+　echo $_POST["Email"];
+?>
+
+<?php
+
+　echo $_POST["Questions"];
+?>
+
+<?php
+
+　echo $_POST["Comments"];
+?>
+
+<?php
+
+　echo $_POST["Name"];
+?>
+
+<?php
+
+　echo $_POST["YearsOld"];
+?>
